e2e6709843feb5b808ac67d3499caf035bab5be2
sanjayjadhav48 <sanjayjadhav48@gmail.com>
2020-02-19 19:35:49 +0530

    Final Code Commit

---
+++ EmployeeInfo/Controllers/HomeController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeInfo.Models;
+using PagedList;
+using PagedList.Mvc;
+
+     
+
+namespace EmployeeInfo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private EmployeeDbContext dbContext = new EmployeeDbContext();
+        //private pagesize = 
+
+        public ActionResult Index(string sortOrder, string SearchString, int? page)
+        {
+            var employees = dbContext.Employees.ToList(); 
+            employees = SearchEmployees(employees, SearchString);
+            employees = SortEmployees(employees, sortOrder);
+            return View(employees.ToPagedList(page ?? 1, 3));
+        }
+
+        [NonAction]
+        private List<Employee> SearchEmployees(List<Employee> employees, string query)
+        {
+            if (!String.IsNullOrEmpty(query) || !String.IsNullOrWhiteSpace(query))
+            {
+                employees = employees.Where(e => e.Name.Contains(query) ||
+                                                e.Branch.Contains(query) ||
+                                                e.Department.Contains(query)).ToList();
+            }
+            return employees;
+        }
+
+        [NonAction]
+        private List<Employee> SortEmployees(List<Employee> employees, string SortOrder)
+        {
+            string SortBy = "";
+            //if (String.IsNullOrEmpty(SortOrder))
+            //{
+            //    SortBy = ViewBag.IdSort = "Id asc"; 
+            //}
+            //else if (SortOrder.Contains("Id"))
+            //{
+            //    SortBy = ViewBag.IdSort = (ViewBag.IdSort == "Id asc") ? "Id desc" : "Id asc"; 
+            //} 
+            //else if (SortOrder.Contains("Name"))
+            //{
+            //    SortBy = ViewBag.NameSort = (ViewBag.NameSort == "Name asc") ? "Name desc" : "Name asc";
+            //}
+            //else if (SortOrder.Contains("Dept"))
+            //{
+            //    SortBy = ViewBag.DeptSort = (ViewBag.DeptSort == "Dept asc") ? "Dept desc" : "Dept asc";
+            //}
+            //else if (SortOrder.Contains("Branch"))
+            //{
+            //    SortBy = ViewBag.BranchSort = (ViewBag.BranchSort == "Branch asc") ? "Branch desc" : "Branch asc";
+            //}  
+
+            if (String.IsNullOrEmpty(SortOrder))
+            {
+                TempData["LastSort"] = SortBy = "Id asc";
+            }
+            else if (SortOrder.Contains("Id"))
+            {
+                TempData["LastSort"] = SortBy = ( (string)(TempData["LastSort"]) == "Id asc") ? "Id desc" : "Id asc";
+            }
+            else if (SortOrder.Contains("Name"))
+            {
+                TempData["LastSort"] = SortBy = ((string)(TempData["LastSort"]) == "Name asc") ? "Name desc" : "Name asc";
+            }
+            else if (SortOrder.Contains("Dept"))
+            {
+                TempData["LastSort"] = SortBy = ((string)(TempData["LastSort"]) == "Dept asc") ? "Dept desc" : "Dept asc";
+            }
+            else if (SortOrder.Contains("Branch"))
+            {
+                TempData["LastSort"] = SortBy = ((string)(TempData["LastSort"]) == "Branch asc") ? "Branch desc" : "Branch asc";
+            }
+
+
+            switch (SortBy)
+            {
+                case "Id asc":
+                    employees = employees.OrderBy(e => e.Id).ToList();
+                    break;
+                case "Id desc":
+                    employees = employees.OrderByDescending(e => e.Id).ToList();
+                    break;
+                case "Name asc":
+                    employees = employees.OrderBy(e => e.Name).ToList();
+                    break;
+                case "Name desc":
+                    employees = employees.OrderByDescending(e => e.Name).ToList();
+                    break;
+                case "Dept asc":
+                    employees = employees.OrderBy(e => e.Department).ToList();
+                    break;
+                case "Dept desc":
+                    employees = employees.OrderByDescending(e => e.Department).ToList();
+                    break;
+                case "Branch asc":
+                    employees = employees.OrderBy(e => e.Branch).ToList();
+                    break;
+                case "Branch desc":
+                    employees = employees.OrderByDescending(e => e.Branch).ToList();
+                    break;
+                default:
+                    break;
+            }
+            return employees;
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ EmployeeInfo/Models/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data;
+
+
+namespace EmployeeInfo.Models
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext() : base("DefaultConnection")
+        {
+            Database.SetInitializer<EmployeeDbContext>(new EmployeeDbInit());
+        }
+        public DbSet<Employee> Employees { get; set; } 
+    }
+
+    public class EmployeeDbInit : DropCreateDatabaseIfModelChanges<EmployeeDbContext>
+    {
+        protected override void Seed(EmployeeDbContext context)
+        {
+            IList<Employee> DefaultEmployee = new List<Employee>();
+            DefaultEmployee.Add(new Employee() { Id = 1, Name = "Sanjay", Branch = "Mumbai", Department="IT" });
+            DefaultEmployee.Add(new Employee() { Id = 2, Name = "Pravin", Branch = "Mumbai", Department = "Sales" });
+            DefaultEmployee.Add(new Employee() { Id = 3, Name = "Sachin", Branch = "Pune", Department = "HR" });
+            DefaultEmployee.Add(new Employee() { Id = 4, Name = "Rupali", Branch = "Pune", Department = "Account" });
+            DefaultEmployee.Add(new Employee() { Id = 5, Name = "John", Branch = "Delhi", Department = "Sales" });
+            DefaultEmployee.Add(new Employee() { Id = 6, Name = "Pradipt", Branch = "Delhi", Department = "HR" });
+            DefaultEmployee.Add(new Employee() { Id = 7, Name = "Vijay", Branch = "London", Department = "Account" });
+            DefaultEmployee.Add(new Employee() { Id = 8, Name = "Jay", Branch = "London", Department = "IT" });
+
+            context.Employees.AddRange(DefaultEmployee);
+
+            base.Seed(context);
+
+        }
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string Branch { get; set; }
+    }
+}
